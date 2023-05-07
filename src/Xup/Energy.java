e7ecefaf99018e7bec17dcebdf5d149b8bcb0129
Esosa013 <132652896+Esosa013@users.noreply.github.com>
2023-05-07 10:12:41 -0700

    Update Energy.java

--- src/Xup/Energy.java
+++ src/Xup/Energy.java
@@ -1,1 +1,15 @@
+package Xup;
+import java.lang.Math;
 
+public class Energy extends Physics {
+    public float Period(float length)
+    {
+        return 6.28f * (float)Math.sqrt(length/gravity);
+    }
+
+    public float potentialEnergy(float mass,float height)
+    {
+        return mass * gravity * height;
+    }
+  
+}
