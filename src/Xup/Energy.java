e0341762cf32e3c675fc4de1a0fd8db2ee9d2a70
Esse Kingston Prosperity Esse <132103317+TheAllFather@users.noreply.github.com>
2023-05-07 18:00:21 +0100

    Update Energy.java

--- src/Xup/Energy.java
+++ src/Xup/Energy.java
@@ -1,10 +1,1 @@
-package Xup;
-import java.lang.Math;
 
-public class Energy extends Physics {
-
-    public float potentialEnergy(float mass,float height)
-    {
-        return mass * gravity * height;
-    }
-}
